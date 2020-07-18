e4c8a6e8e46e325574e8ddf0c901d7c9375fbb52
Siddharth M <siddharth.m98@gmail.com>
2020-07-18 14:33:44 +0530

    video mebeded

--- videos/src/components/VideoDetail.js
+++ videos/src/components/VideoDetail.js
@@ -5,7 +5,21 @@ const VideoDetail = ({video}) => {
     if(!video){
      return <div>Loading...</div>
     }
-    return <div>{video.snippet.title}</div>;
+
+    const videoSRC = `https://www.youtube.com/embed/${video.id.videoId}`
+
+    return (
+    <div>
+        <div className="ui embed">
+            <iframe src={videoSRC} />
+        </div>
+        <div className="ui segment">
+        <h4 className="ui header">{video.snippet.title}</h4> 
+        <p>{video.snippet.description}</p>
+        </div>
+        
+    </div>
+    );
 } ;
 
 
